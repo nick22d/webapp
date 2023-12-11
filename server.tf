764e2b3509dd69e186a22c97c988cff4f1d18a38
ndoropoulos <ndoropoulos@yahoo.com>
2023-12-11 15:18:55 +0000

    Add SG for the ALB

--- server.tf
+++ server.tf
@@ -1,3 +1,25 @@
-resource "aws_instance" "name" {
-  
+# Create the security group for the ALB
+resource "aws_security_group" "sg_for_alb" {
+    name        = "sg_for_alb"
+  description = "Allow HTTP traffic from the internet."
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP from the world"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_for_alb"
+  }
 }
