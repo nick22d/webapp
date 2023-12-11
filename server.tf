3f8319b719a7735cf50f403a9d0e905d9d803314
ndoropoulos <ndoropoulos@yahoo.com>
2023-12-11 16:17:38 +0000

    Add target group for the ALB

--- server.tf
+++ server.tf
@@ -97,3 +97,22 @@ resource "aws_lb" "alb" {
   }
 }
 
+# Create the target group for the ALB
+resource "aws_lb_target_group" "lb_tg" {
+  name     = "tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+/*
+resource "aws_lb_listener" "listener" {
+    load_balancer_arn = aws_lb.alb.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}*/
