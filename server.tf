64c6ab61d2c04a6c69cf4fa1ae42e14e374781e2
ndoropoulos <ndoropoulos@yahoo.com>
2023-12-11 15:27:08 +0000

    Add ALB and SG

--- server.tf
+++ server.tf
@@ -1,25 +1,40 @@
 # Create the security group for the ALB
 resource "aws_security_group" "sg_for_alb" {
-    name        = "sg_for_alb"
+  name        = "sg_for_alb"
   description = "Allow HTTP traffic from the internet."
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "HTTP from the world"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
+    description = "HTTP from the world"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
     Name = "sg_for_alb"
+  }
+}
+
+# Create the ALB
+resource "aws_lb" "alb" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_for_alb.id]
+  subnets            = [for subnet in aws_subnet.public_subnets : subnet.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = "alb"
   }
 }
