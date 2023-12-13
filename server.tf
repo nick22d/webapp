a2068fd5b3ff0a5e68c0bf54c7b06fadc4af0c22
ndoropoulos <ndoropoulos@yahoo.com>
2023-12-13 15:21:49 +0000

    Made a few changes to ensure that the configuration file is inline with the AWS config

---
+++ server.tf
@@ -0,0 +1,153 @@
+# Create the security group for the ALB
+resource "aws_security_group" "sg_for_alb" {
+  name        = "sg_for_alb"
+  description = "Allow HTTP traffic from the internet."
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP from the world"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_for_alb"
+  }
+}
+
+# Create the security group for the EC2 fleet
+resource "aws_security_group" "sg_for_ec2" {
+  name        = "sg_for_ec2"
+  description = "Allow HTTP traffic from the ALB."
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "HTTP from the ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_for_alb.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg_for_ec2"
+  }
+}
+
+# Create the launch configuration for the ASG
+resource "aws_launch_configuration" "launch_config" {
+  image_id        = local.ami
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.sg_for_ec2.id]
+
+  user_data = <<-EOF
+        #!/bin/bash
+        sudo yum update -y
+        sudo yum install -y httpd
+        sudo systemctl start httpd
+        sudo systemctl enable httpd
+        cd /var/www/html
+        echo "<html><h1>This is my 1st server</h1></html>" > index.html
+        sudo systemctl restart httpd
+
+    EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Create the ASG
+resource "aws_autoscaling_group" "asg" {
+  launch_configuration = aws_launch_configuration.launch_config.name
+  min_size             = 2
+  max_size             = 10
+
+  vpc_zone_identifier = [aws_subnet.private_subnet1.id, aws_subnet.private_subnet2.id]
+
+  target_group_arns = [aws_lb_target_group.lb_tg.arn]
+}
+
+# Create the ALB
+resource "aws_lb" "alb" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_for_alb.id]
+  subnets            = [aws_subnet.public_subnet1.id, aws_subnet.public_subnet2.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = "alb"
+  }
+}
+
+# Create the target group for the ALB
+resource "aws_lb_target_group" "lb_tg" {
+  name     = "tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
+  }
+}
+
+
+# Create a listener for the ALB
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+# Create a listener rule for the ALB
+resource "aws_lb_listener_rule" "lb_rule" {
+  listener_arn = aws_lb_listener.listener.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_tg.arn
+  }
+}
